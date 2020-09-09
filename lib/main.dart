08c9095caca3c6293d1fa8b9414c164e70217c2e
TheinDev <theinnaingoodev@gmail.com>
2020-09-09 22:46:36 +0630

    connection changes listener with strem controller is added.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:listeningnet/net_constant.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  
+
+runApp(NetApp());
+
+}
+
+
+class NetApp extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+
+
+
+}
+
+class NetAppState extends State<NetApp> {
+  @override
+  Widget build(BuildContext context) {
+
+
+    return StreamProvider<NetConstant>(
+
+
+    );
+  }
+
+
+
 }
